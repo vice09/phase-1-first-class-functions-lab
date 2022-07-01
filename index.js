6aef569aea73a4f7a831b6f007b6d460028802f2
Edwin Victor Oduor Odhiambo <oduoredwin43@gmail.com>
2022-07-01 11:09:13 +0300

    commit tree b481693ca2c6939c238d57d8c4558248fdeec77a

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+function recievesAFuntion(callback){
+    return callback()
+}
+function returnsANamedFuntion() {
+    const fn = function(){}
+    return fn
+}
+function returnsAnAnonymousFuntion(){
+    
+}
